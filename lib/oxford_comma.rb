53169eddf4e123b3dbe2544bc9ba28dfa0b1d1b3
Bruce Ruffin <bruffin1221@gmail.com>
2019-07-20 13:56:35 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -8,6 +8,6 @@ def oxford_comma(array)
     puts array.insert(1,", ")
     return array.join("")
   elsif array.length>=3
-    return array[0..-1].join(", ")<<", and #{array[-1]}"
+    return array[0..-2].join(", ")<<", and #{array[-1]}"
   end
 end
